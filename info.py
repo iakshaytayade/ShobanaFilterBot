9b53b3528d2264ab97685d9befd66ac9d4d212b8
iakshaytayade <akshaytayade525@gmail.com>
2025-02-23 04:54:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mn_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002363820788'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+1yl8latQzHFjNDJl'')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+1yl8latQzHFjNDJl')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'False')), False)
 IMDB = is_enabled((environ.get('IMDB', 'False')), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', 'True')), True)
